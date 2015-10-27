dce3a98b98414220e1d215fb493376427684d0d3
keyan <session1@START-Coding-MBP-17.local>
2015-10-27 12:55:18 -0700

    I was done with this on Friday, and I meant to turn this in earlier but was having troubles.

---
+++ click_image.js
@@ -0,0 +1,42 @@
+/**
+ * Created by session1 on 10/21/15.
+ */
+var main = function(){
+        $('#pup1').click(function(){
+            $('#pup1').attr('src','http://www.catster.com/wp-content/uploads/2015/06/bengal-kittens-04.jpg');
+        });
+
+        $('#pup2').click(function(){
+            $('#pup2').attr('src','http://kittensforsale.sg/wp-content/uploads/Persian-Kittens.jpg');
+        });
+
+        $('#pup3').click(function(){
+            $('#pup3').attr('src', 'http://savannahcatprice.com/wp/wp-content/uploads/2014/04/F2B-Female-Savannah-kitten.jpg');
+        });
+
+        $('#pup4').click(function(){
+            $('#pup4').attr('src','http://i.imgur.com/r6ZEg4l.jpg');
+        });
+
+        $('#pup5').click(function(){
+            $('#pup5').attr('src','http://cf.ltkcdn.net/cats/images/std/63751-300x199-Siamesekitten-inblanket.jpg');
+        });
+
+        $('#pup6').click(function(){
+            $('#pup6').attr('src','https://s-media-cache-ak0.pinimg.com/236x/88/29/1f/88291f4119e2e2d8030271ba1041d3c2.jpg');
+        });
+
+        $('#pup7').click(function(){
+            $('#pup7').attr('src','http://41.media.tumblr.com/tumblr_lvlrvcTnXg1r0cc6qo1_500.jpg');
+        });
+
+        $('#pup8').click(function(){
+            $('#pup8').attr('src','https://s-media-cache-ak0.pinimg.com/236x/a8/a8/a2/a8a8a2ea3a7cabf48332e8f9937bfd2d.jpg');
+        });
+
+        $('#pup9').click(function(){
+            $('#pup9').attr('src','http://img14.deviantart.net/2f37/i/2011/327/c/2/russian_blue_kitten_02_by_faeriegem-d4h4dj6.jpg')
+        });
+    }
+;
+$(document).ready(main);
